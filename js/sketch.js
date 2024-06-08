cb6765b61e82b62974c06a0a72369f5f44545483
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-06-08 01:07:01 -0700

    Working on extra artwork

--- js/sketch.js
+++ js/sketch.js
@@ -64,25 +64,62 @@ function draw() {
   
   // Gacha Machine Drawing
   
-  translate(camera.centerX, camera.centerY, camera.centerZ - 500);
+  //translate(camera.centerX, camera.centerY, camera.centerZ - 500);
   //translate(camera.eyeX, camera.eyeY, (camera.eyeZ - 1000));
   
   noStroke();
-  fill(0, 0, 255);
+  fill(255, 0, 0, 255);
   lights();
-  rotateX(-0.2);
-  sphere();
   cylinder(50, 100);
   translate(0, 25, 50);
   box();
-  translate(0, -100, -50);
-  sphere(70);
+  translate(0, 5, 11);
+  fill(255, 255, 255);
+  box(30);
+  translate(0, -5, -11);
+  translate(0, -125, -50);
 
-  rotateX(0.2);
-  translate(0, 75, 0);
+  translate(0, 10, 0);
+  fill(255, 255, 0);
+  sphere(15);
+  translate(0, -10, 0);
+
+  translate(0, -50, 0);
+  fill(255, 0, 255);
+  sphere(15);
+  translate(0, 50, 0);
+
+  translate(-20.29, 0, 45.7);
+  fill(0, 255, 255);
+  sphere(15);
+  translate(20.59, 0, -45.7);
+
+  translate(-9.93, -21.74, 43.92);
+  fill(0, 255, 0);
+  sphere(15);
+  translate(9.93, 21.74, -43.92);
+
+  translate(-27.49, 25.86, 32.8);
+  fill(0, 0, 255);
+  sphere(15);
+  translate(27.49, -25.86, -32.8);
+
+  translate(34.43, -32.88, 15.28);
+  fill(0, 150, 150);
+  sphere(15);
+  translate(-34.43, 32.88, -15.28);
+
+  translate(-28.69, -29.68, 28.22);
+  fill(150, 150, 0);
+  sphere(15);
+  translate(28.69, 29.68, -28.22);
+
+  fill(255, 255, 255, 100);
+  sphere(70);
+  translate(0, 125, 50);
   //translate(-camera.eyeX, -camera.eyeY, -(camera.eyeZ - 1000));
-  translate(-camera.centerX, -camera.centerY, -camera.centerZ - 500);
+  //translate(-camera.centerX, -camera.centerY, -camera.centerZ - 500);
   
   orbitControl();
-  model(shape);
+  //model(shape);
 }
