951722a2b98e40f444fa2bd1dda1810bf73b543e
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-06-08 23:24:42 -0700

    gacha machine design

--- js/sketch.js
+++ js/sketch.js
@@ -66,7 +66,7 @@ function draw() {
   
   //translate(camera.centerX, camera.centerY, camera.centerZ - 500);
   //translate(camera.eyeX, camera.eyeY, (camera.eyeZ - 1000));
-  
+  push();
   noStroke();
   fill(255, 0, 0, 255);
   lights();
@@ -79,44 +79,226 @@ function draw() {
   translate(0, -5, -11);
   translate(0, -125, -50);
 
-  translate(0, 10, 0);
+  translate(13.33, -12.69, 46.49);
   fill(255, 255, 0);
   sphere(15);
-  translate(0, -10, 0);
+  translate(-13.33, 12.69, -46.49);
 
-  translate(0, -50, 0);
+  translate(1.88, -37.26, 33.29);
   fill(255, 0, 255);
   sphere(15);
-  translate(0, 50, 0);
+  translate(-1.88, 37.26, -33.29);
 
-  translate(-20.29, 0, 45.7);
+  translate(34.38, -30.29, 20.01);
   fill(0, 255, 255);
   sphere(15);
-  translate(20.59, 0, -45.7);
+  translate(-34.38, 30.29, -20.01);
 
-  translate(-9.93, -21.74, 43.92);
+  translate(37.34, 22.93, 24.08);
   fill(0, 255, 0);
   sphere(15);
-  translate(9.93, 21.74, -43.92);
+  translate(-37.34, -22.93, -24.08);
 
-  translate(-27.49, 25.86, 32.8);
+  translate(12.83, 27.08, 40.02);
   fill(0, 0, 255);
   sphere(15);
-  translate(27.49, -25.86, -32.8);
+  translate(-12.83, -27.08, -40.02);
 
-  translate(34.43, -32.88, 15.28);
+  translate(38.45, -3.47, 31.77);
   fill(0, 150, 150);
   sphere(15);
-  translate(-34.43, 32.88, -15.28);
+  translate(-38.45, 3.47, -31.77);
 
-  translate(-28.69, -29.68, 28.22);
+  translate(-9.52, 10.77, 47.89);
   fill(150, 150, 0);
   sphere(15);
-  translate(28.69, 29.68, -28.22);
+  translate(9.52, -10.77, -47.89);
+//
+  translate(-17.15, -18.04, 43.36);
+  fill(150, 0, 150);
+  sphere(15);
+  translate(17.15, 18.04, -43.36);
+
+  translate(-24.38, -37.39, 22.53);
+  fill(0, 150, 150);
+  sphere(15);
+  translate(24.38, 37.39, -22.53);
+
+  translate(-42.59, -6.15, 25.46);
+  fill(0, 150, 150);
+  sphere(15);
+  translate(42.59, 6.15, -25.46);
+
+  translate(-45.11, 21.25, 3.72);
+  fill(150, 150, 150);
+  sphere(15);
+  translate(45.11, -21.25, -3.72);
+
+  translate(-9.76, 39.19, 29.47);
+  fill(150, 0, 150);
+  sphere(15);
+  translate(9.76, -39.19, -29.47);
+
+  translate(-32.47, 22.36, 30.75);
+  fill(250, 0, 150);
+  sphere(15);
+  translate(32.47, -22.36, -30.75);
+
+  translate(18.84, 35.44, 8.95);
+  fill(250, 50, 250);
+  sphere(15);
+  translate(-18.84, -35.44, -8.95);
+
+  translate(-34.02, -35.42, -9.39);
+  fill(50, 250, 250);
+  sphere(15);
+  translate(34.02, 35.42, 9.39);
+
+  translate(-49.2, -6.86, -5.7);
+  fill(250, 250, 50);
+  sphere(15);
+  translate(49.2, 6.86, 5.7);
+
+  translate(-3.18, -31.3, -38.86);
+  fill(50, 250, 250);
+  sphere(15);
+  translate(3.18, 31.3, 38.86);
+
+  translate(28.24, -28.88, -29.47);
+  fill(255, 50, 0);
+  sphere(15);
+  translate(-28.24, 28.88, 29.47);
+
+  translate(23.48, -43.94, -4.18);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-23.48, 43.94, 4.18);
+//
+  translate(-8.45, -49.28, 0);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(8.45, 49.28, 0);
+
+  translate(46.36, -17.19, -7.43);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-46.36, 17.19, 7.43);
+
+  translate(47.1, 15.26, -7.01);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-47.1, -15.26, 7.01);
+
+  translate(33.71, 16.31, -33.13);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-33.71, -16.31, 33.13);
+
+  translate(33.71, 16.31, -33.13);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-33.71, -16.31, 33.13);
+
+  translate(7.92, 30.19, -39.06);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-7.92, -30.19, 39.06);
+
+  translate(-21.49, 34.3, -29.35);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(21.49, -34.3, 29.35);
+
+  translate(-38.36, 7.51, -32.18);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(38.36, -7.51, 32.18);
+
+  translate(-5.58, 3.95, -49.53);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(5.58, -3.95, 49.53);
+
+  translate(18.82, -4.52, -46.1);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-18.82, 4.52, 46.1);
+
+  translate(-30.33, -21.55, -33.4);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(30.33, 21.55, 33.4);
+
+  translate(-14.7, -26.12, 1.38);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(14.7, 26.12, -1.38);
+
+  translate(15.75, -25.53, 0);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-15.75, 25.53, 0);
+
+  translate(28.23, 0.6, 10.12);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-28.23, -0.6, -10.12);
+
+  translate(16.59, 24.61, 4.35);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-16.59, -24.61, -4.35);
+
+  translate(22.23, 2.92, -19.93);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-22.23, -2.92, 19.93);
+
+  translate(0.96, -15.95, -25.39);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-0.96, 15.95, 25.39);
+
+  translate(-23.87, -1.13, -18.14);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(23.87, 1.13, 18.14);
+
+  translate(-2.61, 17.66, -24.11);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(2.61, -17.66, 24.11);
+
+  translate(-15.31, 25.68, 2.43);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(15.31, -25.68, -2.43);
+
+  translate(-27.33, 0.24, 12.37);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(27.33, -0.24, -12.37);
+
+  translate(2.21, -15.8, 25.4);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(-2.21, 15.8, -25.4);
+
+  translate(-0.51, 14.82, 26.08);
+  fill(55, 250, 0);
+  sphere(15);
+  translate(0.51, -14.82, -26.08);
+
+  fill(55, 250, 0);
+  sphere(15);
 
   fill(255, 255, 255, 100);
+  sphere(66);
+
+  fill(250, 250, 250, 100);
   sphere(70);
   translate(0, 125, 50);
+  pop();
   //translate(-camera.eyeX, -camera.eyeY, -(camera.eyeZ - 1000));
   //translate(-camera.centerX, -camera.centerY, -camera.centerZ - 500);
   
